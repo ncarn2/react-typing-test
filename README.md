2914614088f91f226ade562c14c617c5fc1c2de3
ncarn2 <carnivalnicholas2@gmail.com>
2021-07-24 15:18:25 -0600

    basic component, aquire data from local json, readme update

---
+++ README.md
@@ -0,0 +1,29 @@
+# React Typing Test
+
+## Usage
+
+``` 
+npm install react-typing-test
+```
+
+``` js 
+// something.js
+
+import { TypingTest } from 'react-typing-test';
+
+const Something = (props) => {
+    return (
+        <TypingTest language="english"/>
+    )
+}
+
+export default Something;
+
+```
+
+
+## Configuration
+
+TODO: 
+
+language, word count, timer, themes, ...
--- src/lib/components/TypingTest.js
+++ src/lib/components/TypingTest.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { useEffect } from 'react';
+import './TypingTest.css'
+import wordList from './words.json'
 
 const Button = (props) => {
-    // going to be a bunch of js here probably
+    useEffect(() => {
+        console.log("Once");
+    }, []);
+    
+    const words = wordList.english.map(word => {
+        console.log("Word: ", word)
+        return (
+            <span>{word}</span>
+        );
+    }); 
+
+    // TODO: get text list
+    // TODO: get user input clear on space :)
+    //TODO: get the text file
+    //TODO: check props for language
+    //TODO:
 
     return (
-        <div>
-            <div>
-                asdf
+        <div className="mainContainer">
+            <div className="typingArea">
+                <div>
+                    {words}
+                </div>
+                <div>
+                    <input type="text" spellCheck="false" 
+                           autoComplete="off" className="textInput"/>
+                </div>
             </div>
-            <button>asdfasdf</button>
-            <input />
         </div>
     )
 }

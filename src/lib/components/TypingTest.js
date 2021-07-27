f648f8c803d2fcb7d7d84f7f5187a7ca2c34d596
ncarn2 <carnivalnicholas2@gmail.com>
2021-07-27 17:10:44 -0600

    random words shown on screen, refresh (redo) works

--- src/lib/components/TypingTest.js
+++ src/lib/components/TypingTest.js
@@ -1,40 +1,68 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState } from 'react';
+
 import './TypingTest.css'
 import wordList from './words.json'
 
-const Button = (props) => {
-    useEffect(() => {
-        console.log("Once");
-    }, []);
-    
-    const words = wordList.english.map(word => {
-        console.log("Word: ", word)
+class TypingTest extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.generateNewList = this.generateNewList.bind(this);
+        this.state = { wordsToType: this.getRandomWordList() };
+    }
+
+    generateNewList() {
+        this.setState({
+         wordsToType: this.getRandomWordList()
+        });
+    }
+
+    getRandomWordList() {
+        let wordCount = wordList[this.props.language].length;
+
+        let i = 0;
+
+        let temp = [];
+
+        while (i < this.props.wordLimit) {
+            let word = wordList[this.props.language][Math.floor(Math.random() * wordCount + 1)]
+            temp.push(word);
+            i += 1;
+        }
+
+        return temp.map(word => {
+            return (
+                <span className="spacedWord">{word}</span>
+            );
+        }); 
+    }
+
+    render() {
         return (
-            <span className="spacedWord">{word}</span>
-        );
-    }); 
-
-    // TODO: get text list
-    // TODO: get user input clear on space :)
-    //TODO: get the text file
-    //TODO: check props for language
-    //TODO:
-
-    return (
-        <div className="mainContainer">
-            <div className="typingArea">
-                <div>
-                    {words}
+            <div className="mainContainer">
+                <div className="topBar">
+                    {/* probably some other component might want to 
+                    start splitting this out */}
                 </div>
-                <div>
-                    <input type="text" spellCheck="false" 
-                           autoComplete="off" className="textInput"/>
-                    <button>Redo</button>
+                <div className="typingArea">
+                    <div className="wordArea">
+                        {this.state.wordsToType}
+                    </div>
+                    <div className="bottomBar">
+                        <input type="text" spellCheck="false" 
+                            autoComplete="off" className="textInput"/>
+                        <button onClick={this.generateNewList}>Redo</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default Button;
+TypingTest.defaultProps = {
+    wordLimit: 30,
+    language: 'english',
+}
+
+export default TypingTest; 
